1ad3c67e01f011b752dc77bab10ff2a59b4cb2f4
juyeong04 <yeongisgood@gmail.com>
2023-03-28 18:21:14 +0900

    별 찍기

--- PHP/Practice/06_99_prac_total.php
+++ PHP/Practice/06_99_prac_total.php
@@ -16,8 +16,42 @@ $arr_base = array(1, 2, 3, 4, 5);
 // }
 // echo my_len($arr_base);
 
+//==================================================
+
+// 별찍기를 함수로 만들기
+// $num = 6;
+// for ($i=1; $i < $num; $i++) { 
+//         for ($p=0; $p < $i; $p++) { 
+//             echo "*";
+//         }
+//         echo "\n";
+//     }
 
 
+function star($a)
+{
+    for ($b=0; $b < $a; $b++) { 
+        echo "*";
+    }
+    echo "\n";
+}
+
+function print_star_rect($c)
+{
+    for ($i=0; $i < $c; $i++) { 
+        star($c);
+    }
+}
+
+function print_star_tri($d)
+{
+    for ($i=1; $i <= $d; $i++){
+        star($i);
+    }
+}
+
+
+print_star_tri(4);
 
 
 
