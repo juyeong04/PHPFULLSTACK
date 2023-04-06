f67db5fd6e8b84329f2f1cd5f7084b170133b3f3
juyeong04 <yeongisgood@gmail.com>
2023-04-06 18:12:36 +0900

    23-04-06

--- PHP/Practice/999_1_ex1_blackjack.php
+++ PHP/Practice/999_1_ex1_blackjack.php
@@ -25,25 +25,172 @@
 //8. 한번 사용한 카드는 다시 쓸 수 없음
 
 
-class card
-{
-    public function ran_card
-    {
-        $arr_card = array(2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A");
-        $arr_card_all = array(
-                                $arr_card
-                                , $arr_card
-                                , $arr_card
-                                , $arr_card
-        );
-        $card_pick_shape = $arr_card_all[mt_rand(0, 3)];
-        $card_pick_num = $card_pick_shape[mt(0,12)];
-        return $card_pick_num;
-    }
-    
 
-    
+$arr_card_shape = array("h", "s", "c", "d");
+$arr_card_num = array("2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "K", "Q", "J");
+$cnt = (count($arr_card_shape) * count($arr_card_num)) -1;
+$deck = array();
+foreach ($arr_card_num as $num_val) {
+    foreach ($arr_card_shape as $shape_val) {
+        array_push($deck,"$num_val$shape_val");
+    }
 }
+shuffle($deck);
+
+
+// $user = array();
+// $user[] = $deck[$cnt];
+// $cnt--;
+// $user[] = $deck[$cnt];
+// $cnt--;
+// $dealer = array();
+// $dealer[] = $deck[$cnt];
+// $cnt--;
+// $dealer[] = $deck[$cnt];
+
+$user = array();
+for ($i=0; $i < 2; $i++) { 
+    $user[] = $deck[$cnt];
+    $cnt--;
+}
+$dealer = array();
+for ($i=0; $i < 2 ; $i++) { 
+    $dealer[] = $deck[$cnt];
+    $cnt--;
+}
+
+
+
+$user_sum = 0;
+foreach ($user as $val) 
+{
+    if (strpos($val, "J") !== false || strpos($val, "K") !== false || strpos ($val, "Q"))
+    {
+        $user_sum += 10;
+    }
+    elseif (strpos($val, "A") !== false) 
+    {
+        $user_sum += 11;
+    }
+    else
+    {
+        $user_sum += intval(substr($val,0,1));
+    }
+}
+
+$dealer_sum = 0;
+foreach ($dealer as $val) 
+{
+    if (strpos($val, "J") !== false || strpos($val, "K") !== false || strpos ($val, "Q"))
+    {
+        $dealer_sum += 10;
+    }
+    elseif (strpos($val, "A") !== false) 
+    {
+        $dealer_sum += 11;
+    }
+    else
+    {
+        $dealer_sum += intval(substr($val,0,1));
+    }
+}
+
+
+
+if ($user_sum > $dealer_sum) 
+{
+
+    echo $user_sum." ".$dealer_sum."유저승리";
+}
+elseif($user_sum < $dealer_sum)
+{
+    echo $user_sum." ".$dealer_sum."딜러승리";
+}
+elseif($user_sum === $dealer_sum)
+{
+    echo $user_sum." ".$dealer_sum."무승부";
+}
+
+
+
+
+//------- 이중배열도 해보기
+
+// foreach ($deck as $key => $val) {
+    
+// }
+
+
+// print_r($deck[$cnt]);
+// $cnt--;
+// print_r($deck[$cnt]);
+// $cnt--;
+
+
+// class card
+// {
+//     public function ran_card()
+//     {
+//         $str_k = 11;
+//         $str_q = 12;
+//         $str_j = 13;
+//         $str_a = 14;
+//         $arr_card = array(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+//         $arr_card_all = array(
+//                                 $arr_card
+//                                 , $arr_card
+//                                 , $arr_card
+//                                 , $arr_card
+//         );
+//         $card_pick_shape = $arr_card_all[mt_rand(0, 3)];
+//         $card_pick_num = $card_pick_shape[mt_rand(0,12)];
+//         return $card_pick_num; 
+//     }
+    
+    
+// }
+// $obj_card = new card;
+// $result = $obj_card->ran_card();
+
+// var_dump($result);
+
+// for(i=0; $i <= 5; $i++) {
+//     $arr[$i] = rand(1,45);
+//     for ($p=0; $p < $i; $p++) { 
+//         if($arr[$i] == $arr[$p])
+//         {
+//             $i--;
+//         }
+//     }
+// }
+
+// function card_pick()
+// {
+//     $arr_card_shape = array("h", "s", "c", "d");
+//     $arr_card_num = array("2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "K", "Q", "J");
+//     $deck = array();
+//     foreach ($arr_card_num as $shape_val) {
+//         foreach ($arr_card_shape as $num_val) {
+//             $deck[] = array("shape" => $shape_val, "num" => $num_val);
+//         }
+//     }
+//     return $deck;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
