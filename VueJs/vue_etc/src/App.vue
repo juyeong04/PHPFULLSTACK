edc3579f7d34d54213cb148b381169de5247b800
juyeong04 <yeongisgood@gmail.com>
2023-07-12 16:59:36 +0900

    0712 컴포지션 api

--- VueJs/vue_etc/src/App.vue
+++ VueJs/vue_etc/src/App.vue
@@ -1,20 +1,55 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>App.vue</h1>
+  <PostComponent :propsTest="1"></PostComponent>
+  <hr>
 
+
+  <h2>{{ test }}</h2>
+  <button @click="test = '김김'">김</button>
+  <!-- button 동작 안함! -->
+
+  
+  <h2>{{ data.name }}</h2>
+  <button @click="data.name = '김김2'">김</button><br>
+  
+  <h2>{{ data2.name }}</h2>
+  <button @click="data2.name = 'data2'">data2</button><br>
+  <hr>
+  
   <router-link to="/">홈으로</router-link><br>
   <router-link to="/post">post</router-link>
-
-  <router-view :test="222">
   
+  <router-view :test="222">
+    
   </router-view>
-
+  
 </template>
 
 <script>
+import PostComponent from './components/PostComponent.vue';
+import {ref, reactive} from 'vue';
 
 export default {
   name: 'App',
+  components: {
+    PostComponent,
+  },
+  setup() { //created
+    let data = ref({name: '홍길동', age: 20}); // 데이터 쓸려면 ref안에 선언해야됨
+    let data2 = reactive({name: '철수', age:5});
+    let test = 'aaa'; // 데이터 바인딩 안됨! 실시간 업데이트 안됨
+
+    onMounted(() => {
+      // 처리
+    }),
+    onBeforeMount(() => {
+      // 처리
+    }),
+
+    return { test, data, data2};
+  },
+
 }
 </script>
 
--- VueJs/vue_etc/src/components/PostComponent.vue
+++ VueJs/vue_etc/src/components/PostComponent.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
         <h1>PostComponent.vue</h1>
-        <p>{{ test }}</p>
+        <!-- <p>{{ test }}</p> -->
+        <p>{{ propsTest }}</p>
     </div>
 </template>
 <script>
+import { ref, toRefs } from 'vue';
+
 export default {
     name: 'PostComponent',
     props: {
-        test: Number,
+        // test: Number,
+        propsTest: Number,
+    },
+    setup(props) {
+        let data = ref([]);
+        let {propsTest} = toRefs(props);
+        console.log(propsTest.value);
+
+        return{data};
     }
 }
 </script>
