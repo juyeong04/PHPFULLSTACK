9342549b48662ea303504d662e1ee607164b3a99
juyeong04 <yeongisgood@gmail.com>
2023-07-12 15:47:26 +0900

    0712 vue router

---
+++ VueJs/vue_etc/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>App.vue</h1>
+
+  <router-link to="/">홈으로</router-link><br>
+  <router-link to="/post">post</router-link>
+
+  <router-view :test="222">
+  
+  </router-view>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueJs/vue_etc/src/components/PostComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>PostComponent.vue</h1>
+        <p>{{ test }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'PostComponent',
+    props: {
+        test: Number,
+    }
+}
+</script>
+<style>
+    
+</style>
